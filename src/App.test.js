08230d98c25ca9ff01bac1ec3e0da9f10352a5f3
Himansi-dudhat <139256462+Himansi-dudhat@users.noreply.github.com>
2025-06-26 15:37:29 +0530

    Fix: Update App.test.js for new design

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/welcome to my elegant react site/i);
+  expect(heading).toBeInTheDocument();
 });
+
